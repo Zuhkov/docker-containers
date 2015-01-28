c34b5fa63d01927aed21c366a68f676dc9e6db6b
pmosbach <peter.c.mosbacher@nasa.gov>
2015-01-28 01:21:52 -0600

    Update wallabag

--- wallabag/firstrun.sh
+++ wallabag/firstrun.sh
@@ -5,7 +5,7 @@ if [ -f /var/www/wallabag/db/poche.sqlite ]; then
   echo "Using existing sqlite database file."
 else
   echo "Creating config from template."
-  mv /var/www/wallabag/firstrun/. /var/www/wallabag/db/
+  cp -r /var/www/wallabag/firstrun/. /var/www/wallabag/db/
   chown www-data:www-data -R /var/www/wallabag/db
   chmod 755 -R /var/www/wallabag/db
 fi
