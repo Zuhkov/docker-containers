5b434aeaec76fdbcf9b1970b6f2a92a56c6a9c12
Nephelo <nephelo@pilleslife.de>
2018-04-27 14:39:46 +0200

    Persist paperwork /storage folder

--- firstrun.sh
+++ firstrun.sh
@@ -10,8 +10,18 @@ else
   chown nobody:users /config/database.php
   PW=$(pwgen -1snc 32)
   sed -i -e 's/some_password/'$PW'/g' /config/database.php
+  
+  echo "Preparing LDAP configuration"
+  cp /var/www/paperwork/frontend/app/config/ldap.php /config/ldap.php
+  chown nobody:users /config/ldap.php
+  
+  echo "Preparing storage folder"
+  cp -r /var/www/paperwork/frontend/app/storage/ /config/storage/
+  rm -r /var/www/paperwork/frontend/app/storage/*
 fi
 
+ln -s /config/storage /var/www/paperwork/frontend/app/storage
 ln -s /config/database.php /var/www/paperwork/frontend/app/config/database.php
+ln -s /config/ldap.php /var/www/paperwork/frontend/app/config/ldap.php
 chown nobody:users -R /var/www/paperwork
 chmod 755 -R /var/www/paperwork
