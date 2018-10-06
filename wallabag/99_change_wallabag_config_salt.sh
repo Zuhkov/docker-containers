1fc30b94597c6ab2890d5929a31abaf89904f3e9
Charles Ng <uberchuckie@gmail.com>
2018-10-06 19:27:27 -0400

    Remove other containers

--- wallabag/99_change_wallabag_config_salt.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-if [ -f /etc/container_environment/WALLABAG_SALT ] ; then
-    OLDHASH=`echo -n wallabagwallabag34gAogagAigJaurgbqfdvqQergvqer | sha1sum | awk '{print $1}'`
-    SALT=`cat /etc/container_environment/WALLABAG_SALT`
-    NEWHASH=`echo -n wallabagwallabag$SALT | sha1sum | awk '{print $1}'`
-    sqlite3 /var/www/wallabag/db/poche.sqlite "UPDATE users SET password='$NEWHASH' WHERE password='$OLDHASH'"
-    sed -i "s/'SALT', '.*'/'SALT', '$SALT'/" /config/config.inc.php
-fi
